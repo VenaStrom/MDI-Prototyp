366bed0022aae6a923e35c91dd91f8da9a351a80
VenaStrom <strom.vena+gh@gmail.com>
2026-03-10 14:28:01 +0100

    add stats url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     tailwindcss(),
@@ -19,4 +19,10 @@ export default defineConfig({
     emptyOutDir: true,
   },
   assetsInclude: ["**/*.svg", "**/*.csv"],
-});
+  define: {
+    STATS_URL: JSON.stringify(mode === "development"
+      ? "http://localhost:3000"
+      : "https://stats.venastrom.se:3000"
+    ),
+  },
+}));
