a6744505bc74210288ec8551929b993a6216ae1b
VenaStrom <strom.vena+gh@gmail.com>
2026-03-10 12:52:21 +0100

    update eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,40 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
+// @ts-check
 
-export default [
-  {
-    ignores: ["dist", "node_modules"],
-  },
-  js.configs.recommended,
+import eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
   {
     files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parser: tsParser,
+    languageOptions:{
       parserOptions: {
         project: "./tsconfig.json",
         tsconfigRootDir: import.meta.dirname,
         ecmaVersion: "latest",
         sourceType: "module",
       },
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-    },
-    rules: {
-      ...tsPlugin.configs["recommended-type-checked"].rules,
-      "no-undef": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/no-unsafe-argument": "off",
-      "@typescript-eslint/no-unnecessary-type-assertion": "off",
-      "@typescript-eslint/restrict-template-expressions": "off",
-    },
+    }
   },
-];
+  globalIgnores([
+    "dist",
+    "node_modules",
+  ]),
+);
